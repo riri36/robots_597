541ec4c903eb204c35e2bde63fc21f4d1d28f1c6
Dexter Siu <dextermsiu@gmail.com>
2025-02-06 18:19:33 -0500

    updated angular error

--- utilities.py
+++ utilities.py
@@ -108,6 +108,8 @@ def calculate_linear_error(current_pose, goal_pose):
     # Compute the linear error in x and y
     # Remember that current_pose = [x,y, theta, time stamp] and goal_pose = [x,y]
     # Remember to use the Euclidean distance to calculate the error.
+
+    # Calculate the error vector x and y position
     error_linear = [goal_pose.x - current_pose.x , goal_pose.y-current_pose.y]
 
     return error_linear
@@ -119,19 +121,29 @@ def calculate_angular_error(current_pose, goal_pose):
     # Remember that current_pose = [x,y, theta, time stamp] and goal_pose = [x,y]
     # Use atan2 to find the desired orientation
     # Remember that this function returns the difference in orientation between where the robot currently faces and where it should face to reach the goal
-    error_linear = [goal_pose.x - current_pose.x , goal_pose.y-current_pose.y]
-    curr_angle = current_pose.theta
 
-    if curr_angle > M_PI:
-        curr_angle = -1*(curr_angle-M_PI)
-        
+    # Calculate the error vectorx and y position
+    error_linear = [goal_pose.x - current_pose.x , goal_pose.y-current_pose.y]
+
+    # #Assumes theta is given from 0 to 2pi
+    # curr_angle = current_pose.theta
+    # if curr_angle > M_PI:
+    #     curr_angle = -1*(curr_angle-M_PI)
+
+    #Assumes theta is give from pi to -pi
+    curr_angle = current_pose.theta
+    
+    # Use the linear error to find the desired angle we want the robot to face
     goal_angle = atan2(error_linear[0],error_linear[1])
 
-    error_angular = goal_angle - curr_angle
+    # Calculate both options for the robot to roatate to goal angle
+    angle_diff = [goal_angle - curr_angle, curr_angle - goal_angle]
 
-    if abs(error_angular) > M_PI:
-        error_angular = curr_angle - goal_angle
-        
+    # Take the smallest angle error and set that to error term
+    if abs(angle_diff[1]) > abs(angle_diff[0]):
+        error_angular = angle_diff[0]
+    else:
+        error_angular = angle_diff[1]
 
     # Remember to handle the cases where the angular error might exceed the range [-π, π]
     
