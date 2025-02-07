f99216c55bccdfc2175c540f9228696ec8c53455
Dexter Siu <dextermsiu@gmail.com>
2025-02-06 19:09:20 -0500

    added odometry initialization and pose data

--- localization.py
+++ localization.py
@@ -19,22 +19,34 @@ class localization(Node):
         # TODO Part 3: Define the QoS profile variable based on whether you are using the simulation (Turtlebot 3 Burger) or the real robot (Turtlebot 4)
         # Remember to define your QoS profile based on the information available in "ros2 topic info /odom --verbose" as explained in Tutorial 3
 
-        odom_qos=...
+        # check if right
+        odom_qos = QoSProfile(reliability=2, durability=2, history=1, depth=10)
         
         self.loc_logger=Logger("robot_pose.csv", ["x", "y", "theta", "stamp"])
         self.pose=None
         
         if localizationType == rawSensor:
         # TODO Part 3: subscribe to the position sensor topic (Odometry)
-        # ...
+            self.create_subscription(odom, '/odom', self.odom_callback, qos_profile=odom_qos)
         else:
             print("This type doesn't exist", sys.stderr)
     
     
-    def odom_callback(self, pose_msg):
+    def odom_callback(self, pose_msg: odom):
         
         # TODO Part 3: Read x,y, theta, and record the stamp
-        self.pose=[ ... ]
+        self.odom_initialized=True
+
+        timestamp = Time.from_msg(pose_msg.header.stamp).nanoseconds
+
+        odom_orientation = pose_msg.pose.pose.orientation
+        odom_pos = pose_msg.pose.pose.position
+        # odom_lin_vel = pose_msg.twist.twist.linear
+        # odom_angular_vel = pose_msg.twist.twist.angular
+        
+        yaw = euler_from_quaternion(odom_orientation)
+        
+        self.pose=[odom_pos.x, odom_pos.y ,yaw , timestamp]
         
         # Log the data
         self.loc_logger.log_values([self.pose[0], self.pose[1], self.pose[2], Time.from_msg(self.pose[3]).nanoseconds])
@@ -43,6 +55,9 @@ class localization(Node):
         return self.pose
 
 # TODO Part 3
+
+#TODO LAB 2
+
 # Here put a guard that makes the node run, ONLY when run as a main thread!
 # This is to make sure this node functions right before using it in decision.py
     
